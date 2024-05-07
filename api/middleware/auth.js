232ee4c4f4e49f9f3a6b8a5c6246748a24eaea43
João Lucas <joao.oliveira307@fatec.sp.gov.br>
2024-05-06 22:21:38 -0300

    Att

---
+++ api/middleware/auth.js
@@ -0,0 +1,20 @@
+import  Jwt  from "jsonwebtoken";
+
+export default async function auth(req, res, next){
+    const token = req.header('access-token')
+    if(!token) return res.status(401).json({ //401-Not Authorized
+        msg: 'Acesso negado. É obriatório o envio do token JWT'
+    })
+    try {
+        const decoded = Jwt.verify(token, process.env.SECRET_KEY)
+        /*O decoded irá conter:
+        playload - id do usuário
+        exp (expiration) - Data de expiração
+        iat (issued at) - Data de criação
+        */
+       req.usuario = await decoded.usuario
+       next() // Direcionamos para o endpoint
+    }catch(e){
+        res.status(403).send({error: `Token inválido: ${e.message}`})
+    }
+}
--- api/routes/prestador.js
+++ api/routes/prestador.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { connectToDatabase } from '../utils/mongodb.js'
 import { check, validationResult} from 'express-validator'
+import auth from '../middleware/auth.js'
 
 const router = express.Router()
 const {db,ObjectId} = await connectToDatabase()
@@ -50,7 +51,7 @@ check('localizacao.coordinates.*').isFloat()
  * Lista todos os prestadores de serviço
  * Parâmetros: limit, skip e order
  */
-router.get('/', async(req, res)=>{
+router.get('/', auth, async(req, res)=>{
     const {limit, skip, order} = req.query //Obter da URL
     try{
         const docs = []
@@ -77,7 +78,7 @@ router.get('/', async(req, res)=>{
  * Parâmetros: id
  */
 
-router.get('/id/:id', async (req, res)=>{
+router.get('/id/:id', auth, async (req, res)=>{
     try{
         const docs = []
         await db.collection(nomeCollection)
@@ -103,7 +104,7 @@ router.get('/id/:id', async (req, res)=>{
  * Parâmetros: id
  */
 
-router.get('/razao/:filtro', async(req, res)=>{
+router.get('/razao/:filtro', auth, async(req, res)=>{
     try{
         const filtro = req.params.filtro.toString()
         const docs = []
@@ -135,7 +136,7 @@ router.get('/razao/:filtro', async(req, res)=>{
  * Remove o prestador de serviço pelo id
  * Parâmetros: id
  */
-router.delete('/:id', async(req, res)=>{
+router.delete('/:id', auth, async(req, res)=>{
     const result = await db.collection(nomeCollection).deleteOne({
         "_id": {$eq: new ObjectId(req.params.id)}
     })
@@ -158,7 +159,8 @@ router.delete('/:id', async(req, res)=>{
  * Parâmetros: Objeto prestador
  */
 
-router.post('/', validaPrestador, async(req, res) =>{
+router.post('/', auth, validaPrestador, async(req, res) =>{
+    req.body.usuarioInclusao = req.usuario.id
     try{
         const errors = validationResult(req)
         if(!errors.isEmpty()){
@@ -177,7 +179,7 @@ router.post('/', validaPrestador, async(req, res) =>{
  * Altera um prestador de serviço pelo _id
  * Parâmetros: Objeto prestador
  */
-router.put('/', validaPrestador, async(req, res) => {
+router.put('/', auth, validaPrestador, async(req, res) => {
     let idDocumento = req.body._id // Armazena o id do documento
     delete req.body._id //Removemos o _id do body que foi recebido na req.
     try{
