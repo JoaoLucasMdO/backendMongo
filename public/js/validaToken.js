7e0afbf848d86269ad3f53a2cebd3abfae90f152
João Lucas <joao.oliveira307@fatec.sp.gov.br>
2024-05-13 19:37:31 -0300

    Att

---
+++ public/js/validaToken.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const token = localStorage.getItem('token')
+    if(!token){//Não existe o token, enviamos para o login
+        window.location.href = 'index.html'
+    }else{
+        //token existe, Verificar se é válido
+        const tokenData = parseJwt(token)
+        if(tokenData && tokenData.exp && tokenData.exp * 1000 > Date.now()){
+            //Token existe e não expirou. Não faremos nada
+        } else{
+            window.location.href = 'index.html' //Se estiver expirado
+        }
+    }
+})
+
+function parseJwt(token){
+    try{
+        const base64Url = token.split(".")[1]
+        const base64 = base64Url.replace('-','+').replace('_','/')
+        return JSON.parse(atob(base64))
+    }catch(e){
+        return null
+    }
+}
